899f75b4d7c26661c83047fd80b3a7491be776c8
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-08-27 14:15:29 +0300

    add active state

--- source/js/index.js
+++ source/js/index.js
@@ -1,15 +1,12 @@
 const mainNav = document.querySelector(".page-header__main-nav");
 const navToggle = document.querySelector(".page-header__toggle");
 const navClose = document.querySelector(".page-header__close");
-console.log("wow");
 
 navToggle.addEventListener("click", e => {
-  console.log("here");
   if (mainNav.classList.contains("page-header__closed")) {
     mainNav.classList.remove("page-header__closed");
     mainNav.classList.add("page-header__opened");
   } else {
-    console.log("there");
     mainNav.classList.add("page-header__closed");
     mainNav.classList.remove("page-header__opened");
   }
@@ -20,3 +17,21 @@ navClose.addEventListener("click", () => {
     mainNav.classList.remove("page-header__opened");
   }
 });
+const currentPage = window.location.href.split("/")[3];
+if (window.innerWidth > 768) {
+  if (currentPage === "form.html") {
+    document
+      .querySelector(".page-header__element--feedback a")
+      .classList.add("page-header__element-link--active");
+  }
+  if (currentPage === "index.html") {
+    document
+      .querySelector(".page-header__element--main a")
+      .classList.add("page-header__element-link--active");
+  }
+  if (currentPage === "photo.html") {
+    document
+      .querySelector(".page-header__element--photo a")
+      .classList.add("page-header__element-link--active");
+  }
+}
