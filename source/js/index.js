fd92489bc5be911e1349e0bb8baf82f710837db7
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-08-27 14:26:01 +0300

    fix menu buttons on mobile

--- source/js/index.js
+++ source/js/index.js
@@ -2,11 +2,15 @@ const mainNav = document.querySelector(".page-header__main-nav");
 const navToggle = document.querySelector(".page-header__toggle");
 const navClose = document.querySelector(".page-header__close");
 
+console.log(navToggle);
 navToggle.addEventListener("click", e => {
   if (mainNav.classList.contains("page-header__closed")) {
+    console.log("open");
     mainNav.classList.remove("page-header__closed");
     mainNav.classList.add("page-header__opened");
+    navToggle.classList.add("visually-hidden");
   } else {
+    console.log("close");
     mainNav.classList.add("page-header__closed");
     mainNav.classList.remove("page-header__opened");
   }
@@ -15,6 +19,7 @@ navClose.addEventListener("click", () => {
   if (!mainNav.classList.contains("page-header__closed")) {
     mainNav.classList.add("page-header__closed");
     mainNav.classList.remove("page-header__opened");
+    navToggle.classList.remove("visually-hidden");
   }
 });
 const currentPage = window.location.href.split("/")[3];
