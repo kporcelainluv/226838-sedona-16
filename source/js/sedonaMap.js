7704617f976d7c24c7dbca01ce6bdd623e60dae9
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-08-28 12:34:35 +0300

    add no-js map on index

---
+++ source/js/sedonaMap.js
@@ -0,0 +1,8 @@
+const sedonaMapNoJs = document.querySelector(".main-contact__map");
+const sedonaDigitalMap = document.querySelector(".main-contact__map--digital");
+const mapMarker = document.querySelector(".main-contact__map-wrap");
+
+sedonaMapNoJs.classList.add("visually-hidden");
+sedonaMapNoJs.style.width = "1px";
+sedonaDigitalMap.classList.remove("visually-hidden");
+mapMarker.classList.add("visually-hidden");
